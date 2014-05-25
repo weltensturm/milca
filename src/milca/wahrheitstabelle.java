4c8889cecf5317c673b66187515f7ffd4b2e3c60
weltensturm <weltensturm@gmail.com>
2014-05-25 18:44:31 +0200

    KNF zusammengefügt, Wahrheitstabelle überarbeitet

--- src/milca/wahrheitstabelle.java
+++ src/milca/wahrheitstabelle.java
@@ -1,42 +1,22 @@
 package milca;
 
+
 public class Wahrheitstabelle {
+	
 	public boolean [][] tabelle;
-    private int next;
-    
-	public int getNext() {
-		return next;
-	}
-	public void setNext(int next) {
-		this.next = next;
-	}
-    
-	public Wahrheitstabelle(int zahl)
-    {
-        if(zahl <=6 && zahl >0)
-            tabelle= new boolean [(int) Math.pow(2d,zahl)][zahl];
-        else
-        {
-            System.out.println("Falsche werte bei zahl");
-            tabelle= new boolean [(int) Math.pow(2d,zahl)][6];
-        }
+
+	public Wahrheitstabelle(int variables) throws Exception {
+        if(variables > 6 || variables <= 0)
+        	throw new Exception("Only 1-6 variables allowed, got " + variables);
+        tabelle = new boolean[(int)Math.pow(2, variables)][variables];
     }
 
-	public void makeTabelleV()
-    {
-        int h = tabelle[0].length;
-        for(int i =0; i < tabelle.length; i++)
-        {
-            int k =0;
-            for(int z=h-1; z >= 0; z--)
-            {
-                if((i/(int) Math.pow(2, z))%2==1)
-                    tabelle[i][k]=true;
-                else
-                    tabelle[i][k]=false;
-                k++;
-            }
-        }
+	public void makeTabelleV(){
+		for(int i=0; i<tabelle.length; i++){
+			for(int j=0; j<tabelle[i].length; j++){
+				tabelle[i][j] = ((i >> j) & 1) == 1;
+			}
+		}
     }
 	
 }
